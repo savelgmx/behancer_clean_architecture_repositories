58011b7ef75bc019a19728ae2fa0a9d6bacd9250
savelgmx <savel@gmx.com>
2019-12-17 16:27:12 +0700

    Есть интерфейс ProfileRepository //TODO Создать ProfileServer и ProfileDB репозитории

---
+++ domain/src/main/java/com/elegion/domain/repository/ProfileRepository.java
@@ -0,0 +1,7 @@
+package com.elegion.domain.repository;
+
+public interface ProfileRepository {
+    public static final String SERVER = "SERVER";
+    public static final String DB = "DB";
+    //здесь должны быть функции получения user get user
+}
