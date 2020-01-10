402525a33e723aca3b4bfe8fa5b280f25b3b3862
savelgmx <savel@gmx.com>
2020-01-10 15:48:45 +0700

    ProfileRepository добавлен в RepositoryModule
    //TODO две реализации
                                                                                              ProfileServerRepository и ProfileDBRepository

--- data/src/main/java/com/elegion/data/repository/ProfileServerRepository.java
+++ data/src/main/java/com/elegion/data/repository/ProfileServerRepository.java
@@ -1,6 +1,33 @@
 package com.elegion.data.repository;
 
+import com.elegion.data.api.BehanceApi;
+import com.elegion.domain.model.user.User;
+import com.elegion.domain.model.user.UserResponse;
 import com.elegion.domain.repository.ProfileRepository;
 
+import javax.inject.Inject;
+
+import io.reactivex.Single;
+import io.reactivex.functions.Function;
+
 public class ProfileServerRepository implements ProfileRepository {
+
+    @Inject
+    BehanceApi mApi;
+    private String username;
+
+    @Inject
+    public ProfileServerRepository(){}
+
+    @Override
+    public Single<User> getUser(){
+        return mApi.getUserInfo(username).map(new Function<UserResponse, User>() {
+            @Override
+            public User apply(UserResponse userResponse) throws Exception {
+                return null;
+            }
+        });
+    }
+
+
 }
--- domain/src/main/java/com/elegion/domain/repository/ProfileRepository.java
+++ domain/src/main/java/com/elegion/domain/repository/ProfileRepository.java
@@ -1,7 +1,12 @@
 package com.elegion.domain.repository;
 
+import com.elegion.domain.model.user.User;
+
+import io.reactivex.Single;
+
 public interface ProfileRepository {
     public static final String SERVER = "SERVER";
     public static final String DB = "DB";
-    //здесь должны быть функции получения user get user
+
+    Single<User> getUser();
 }
