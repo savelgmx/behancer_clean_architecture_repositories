eec6ab8287e3cb4c26a3595570ea925c182b66d8
savelgmx <savel@gmx.com>
2020-01-10 18:56:39 +0700

    Исправлена ошибка в ProfileDBRepositary //TODO доработать getUser d ProfileDBRepository

--- data/src/main/java/com/elegion/data/repository/ProfileDBRepository.java
+++ data/src/main/java/com/elegion/data/repository/ProfileDBRepository.java
@@ -1,6 +1,13 @@
 package com.elegion.data.repository;
 
+import com.elegion.domain.model.user.User;
 import com.elegion.domain.repository.ProfileRepository;
 
+import io.reactivex.Single;
+
 public class ProfileDBRepository implements ProfileRepository {
+    @Override
+    public Single<User> getUser() {
+        return null;
+    }
 }
