45f808b71b2b81844daeb47673aab2f563eb0d04
savelgmx <savel@gmx.com>
2020-01-17 15:03:03 +0700

    New branch ProfileService In AppComponent added ServiceModule class //TODO profile presenter В ProfilePresenter убрать Storage и Api

--- presentation/src/main/java/com/elegion/test/behancer/di/AppComponent.java
+++ presentation/src/main/java/com/elegion/test/behancer/di/AppComponent.java
@@ -12,7 +12,7 @@ import dagger.Component;
  */
 
 @Singleton
-@Component(modules = {AppModule.class, NetworkModule.class, RepositoryModule.class})
+@Component(modules = {AppModule.class, NetworkModule.class, RepositoryModule.class,ServiceModule.class})
 public interface AppComponent {
 
     void inject(ProjectsFragment injector);
